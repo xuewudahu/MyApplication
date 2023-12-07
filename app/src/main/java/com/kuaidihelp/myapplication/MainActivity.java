ecec64cc2a69ad5ae15d2fb2d8defa377ee849f6
xuewudahu <56527841+xuewudahu@users.noreply.github.com>
2023-12-07 18:12:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/kuaidihelp/myapplication/MainActivity.java
+++ app/src/main/java/com/kuaidihelp/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent serviceIntent = new Intent(this, MyService.class);
         startForegroundService(serviceIntent);
-
+//服务器提交的代码
 
 
      //   Log.e("wxw","---------"+ encodeForURL("http://192.168.0.101:8080/chukuyi/device/updateDeviceInfo.do?token=cc1323a2efe0422db65f28e532938cb61693360937367&device={\"userId\": \"00309d73c43d4b3fb628168e7680646f\",\"sn\":\"123456\",\"availableDeviceCapacity\":\"20G\",\"bluetoothMAC\":\"33:44:55:52:74\",\"deviceCapacity\":\"32G\",\"iccid\":\"\", \"imei\": \"8651245622500001\",\"isActivationLockEnabled\":true,\"isDeviceLocatorServiceEnabled\":true,\"meid\":\"A65124562250001\",\"model\":\"m1\",\"modelName\":\"W201\",\"wifiMac\":\"33:44:55:34:57\",\"location\":\"浙江省杭州市西湖区 万塘路252号计量大厦409\"}"));
